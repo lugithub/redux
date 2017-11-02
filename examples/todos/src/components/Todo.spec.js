0b86b063948c253939bda0f39b81267977bca2ce
bin lu <lu.bin.lu.bin@gmail.com>
2017-11-02 16:22:37 -0400

    test Todo style

---
+++ examples/todos/src/components/Todo.spec.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai';
+import React from 'react'
+import Todo from './Todo'
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+describe('Todo component', () => {
+  it('renders a <li/> component', () => {
+    const onClick = sinon.spy();
+    const wrapper = shallow(<Todo
+      text={'test'}
+      completed={false} onClick={onClick} />);
+
+    expect(wrapper.find('li')).to.have.lengthOf(1);
+    expect(wrapper.find('li').prop('style')).to.eql({
+      textDecoration: 'none'
+    });
+  });
+
+  it('renders a line-through <li/> component', () => {
+    const onClick = sinon.spy();
+    const wrapper = shallow(<Todo
+      text={'test'}
+      completed={true} onClick={onClick} />);
+
+    expect(wrapper.find('li').prop('style')).to.eql({
+      textDecoration: 'line-through'
+    });
+  });
+})
