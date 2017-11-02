d762238f183aaf43a2c36100d7b5c69ac9260846
bin lu <lu.bin.lu.bin@gmail.com>
2017-11-02 15:45:25 -0400

    test click is so easy with enzyme

---
+++ examples/todos/src/components/Link.enzyme.spec.js
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import React from 'react'
+//import { Provider } from 'react-redux'
+//import { createStore } from 'redux'
+//import TestRenderer from 'react-test-renderer'
+//import reducer from '../reducers'
+import Link from './Link'
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+
+describe('Link component', () => {
+  it('renders an <a/> component', () => {
+    const wrapper = shallow(<Link active={false} onClick={ e => {
+        console.log('onClick', e);
+      }}>children</Link>);
+    expect(wrapper.find('a')).to.have.lengthOf(1);
+  });
+
+  it('should handle click events', () => {
+    const onClick = sinon.spy();
+    const preventDefault = sinon.spy();
+    const wrapper = shallow(<Link active={false} onClick={onClick}>
+      children</Link>);
+    wrapper.find('a').simulate('click', { preventDefault });
+    expect(onClick.called).to.be.true;
+    expect(preventDefault.called).to.be.true;
+  });
+})
