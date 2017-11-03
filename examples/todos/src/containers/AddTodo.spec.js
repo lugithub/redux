4c63a07a283de5019462c73e09521ee23e9debba
lugithub <lu.bin.lu.bin@gmail.com>
2017-11-03 00:10:01 -0400

    test container component with enzyme shallow and dive but shallow does not support ref so i have to try with mount once enzyme 3 is released

--- examples/todos/src/containers/AddTodo.spec.js
+++ examples/todos/src/containers/AddTodo.spec.js
@@ -1,40 +1,25 @@
 import { expect } from 'chai';
 import React from 'react'
 import AddTodo from './AddTodo'
-import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import sinon from 'sinon';
-import { JSDOM } from 'jsdom'
-const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
-const { window } = jsdom;
-
-function copyProps(src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .map(prop => Object.getOwnPropertyDescriptor(src, prop));
-  Object.defineProperties(target, props);
-}
-
-global.window = window;
-global.document = window.document;
-global.navigator = {
-  userAgent: 'node.js',
-};
-copyProps(window, global);
 
 describe('AddTodo component', () => {
   it('should handle submit', () => {
-    const dispatch = sinon.spy();
+    const dispatch = sinon.stub();
     const store = {
       subscribe: sinon.spy(),
       dispatch,
       getState: sinon.spy(),
     };
 
-    const wrapper = mount(<AddTodo
-      dispatch={{ dispatch }}
-      store={ store } />);
+    const wrapper = shallow(<AddTodo
+      dispatch={ dispatch }
+      store={ store } />).dive();
 
-    //wrapper.find('form').simulate('submit');
-    //expect(dispatch.called).to.be.true;
+    wrapper.find('form').simulate('submit', {
+      preventDefault: sinon.spy()
+    });
+    expect(dispatch.called).to.be.true;
   });
 })
